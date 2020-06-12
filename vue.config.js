96411b6ce7c23867941d78fbd12d0712cae0f6ad
renqingzhong <renqingzhong@hupu.com>
2020-06-12 10:05:56 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
   devServer: {
     'proxy': {
       '/api': {
-        target: 'http://localhost:7001',
+        target: 'http://127.0.0.1:7001',
         pathRewrite: {
           '^/api': '/api',
         },
